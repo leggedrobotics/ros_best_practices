c3a9d023157d639766c8d19aaf5527e188ce30ce
Tom Lankhorst <info@tomlankhorst.nl>
2020-10-23 17:35:53 +0200

    First step to ROS2

--- ros_package_template/src/ros_package_template_node.cpp
+++ ros_package_template/src/ros_package_template_node.cpp
@@ -1,13 +1,16 @@
-#include <ros/ros.h>
+#include "rclcpp/rclcpp.hpp"
+
 #include "ros_package_template/RosPackageTemplate.hpp"
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "ros_package_template");
-  ros::NodeHandle nodeHandle("~");
+  rclcpp::init(argc, argv);
 
-  ros_package_template::RosPackageTemplate rosPackageTemplate(nodeHandle);
+  rclcpp::executors::SingleThreadedExecutor exec;
+  auto node = std::make_shared<ros_package_template::RosPackageTemplate>("ros_package_template");
 
-  ros::spin();
+  exec.add_node(node);
+  exec.spin();
+  rclcpp::shutdown();
   return 0;
 }
